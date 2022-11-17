39f90156ed8513f2f4870e40c02bae3233320aa4
Jan Juracka <xjurack1@fi.muni.cz>
2022-11-17 14:20:22 +0100

    Added build downloader

--- standalone/mu_html.py
+++ standalone/mu_html.py
@@ -2,9 +2,10 @@ from argparse import ArgumentParser
 import os
 import platform
 from pathlib import Path
-from shutil import rmtree
+from shutil import rmtree, unpack_archive
+from typing import Any
 import requests
-
+import json
 
 _BINARY_URLS = {'Linux': 'https://github.com/somik861/mu_cmake/releases/download/MU_binaries/mu_linux64_gcc8.tar.gz',
                 'Windows': 'https://github.com/somik861/mu_cmake/releases/download/MU_binaries/mu_win64_cygwin.zip'}
@@ -23,28 +24,68 @@ SVGTEX_BINARY_NAME = _SVGTEX_BINARY_NAMES[platform.system()]
 FILES_FOLDER_PATH = Path(os.path.join(SCRIPT_FOLDER, FILES_FOLDER_NAME))
 MU_BINARY_PATH = Path(os.path.join(FILES_FOLDER_PATH, MU_BINARY_NAME))
 SVGTEX_BINARY_PATH = Path(os.path.join(FILES_FOLDER_PATH, SVGTEX_BINARY_NAME))
+CONFIG_PATH = Path(os.path.join(FILES_FOLDER_PATH, 'config.json'))
+
+
+class utils:
+    @staticmethod
+    def get_url_filename(url: str) -> str:
+        return url.rsplit('/', 1)[-1]
+
+    @staticmethod
+    def load_config() -> dict[str, Any]:
+        return json.load(open(CONFIG_PATH, 'r'))  # type: ignore
+
+    @staticmethod
+    def save_config(cfg: dict[str, Any]) -> None:
+        json.dump(cfg, open(CONFIG_PATH, 'w'), indent=4)
 
 
 def _validate_files() -> bool:
+    try:
+        assert FILES_FOLDER_PATH.exists()
+        assert MU_BINARY_PATH.exists()
+        assert SVGTEX_BINARY_PATH.exists()
+        assert CONFIG_PATH.exists()
+
+        cfg = utils.load_config()
+        assert 'version' in cfg
+        assert cfg['version'] == BINARY_URL or cfg['version'] == 'custom'
+    except AssertionError:
+        return False
+
     return True
 
 
 def _clean_download() -> None:
+    print('Removing old files...')
     rmtree(FILES_FOLDER_PATH, ignore_errors=True)
 
     FILES_FOLDER_PATH.mkdir(parents=True, exist_ok=True)
 
+    print('Downloading binaries...')
+    filepath = os.path.join(
+        FILES_FOLDER_PATH, utils.get_url_filename(BINARY_URL))
     data = requests.get(BINARY_URL).content
+    open(filepath, mode='wb').write(data)
+
+    print('Unpacking data...')
+    unpack_archive(filepath, extract_dir=FILES_FOLDER_PATH)
+
+    cfg: dict[str, Any] = {}
+    cfg['version'] = BINARY_URL
+    utils.save_config(cfg)
+
+    print('Cleaning temporary files...')
+    Path(filepath).unlink()
 
     assert _validate_files(), 'Files werent correctly downloaded,'
     'please report this at https://github.com/somik861/mu_interactive'
 
 
 def init_if_needed() -> None:
-    print(f'{MU_BINARY_PATH=}')
-    print(f'{SVGTEX_BINARY_PATH=}')
-    print(f'{SCRIPT_FOLDER=}')
-    print(f'{FILES_FOLDER_PATH=}')
+    if not _validate_files():
+        _clean_download()
 
 
 def main(inp: str, out: str) -> None:
